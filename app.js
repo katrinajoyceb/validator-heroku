1986bafd85c9044af7963c5e3c24429177a2bf1c
katrinajoyceb <44687814+katrinajoyceb@users.noreply.github.com>
2020-09-30 00:23:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
