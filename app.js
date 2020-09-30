568515872898981eaf23e3903de5ed290bf1e239
katrinajoyceb <44687814+katrinajoyceb@users.noreply.github.com>
2020-09-30 00:26:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -61,6 +61,20 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
